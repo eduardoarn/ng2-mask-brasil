670a2aa56639cd7fcf1efa2ae90cb47009b4484e
Eduardo Miranda Soares <eduardo@heds.com.br>
2017-10-26 16:25:05 -0300

    ajuste pipe telefone

--- src/pipe.telefone.ts
+++ src/pipe.telefone.ts
@@ -4,20 +4,35 @@ import * as libphonenumber from 'google-libphonenumber';
 
 @Pipe({ name: 'telefone' })
 export class TelefonePipe implements PipeTransform {
-    transform(value: string): string {
-
+    transform(value: string, tipo: string = 'nacional', country: string = 'BR'): string {
         let PNF = libphonenumber.PhoneNumberFormat;
 
         // Get an instance of `PhoneNumberUtil`.
         let phoneUtil = libphonenumber.PhoneNumberUtil.getInstance();
 
-        // Add a leading '+' sign if not available. Assumption is all numbers contain country code prefixed.
-        let newValue = value.length > 0 && value.charAt(0) != "+" ? "+" + value : value;
+        let tipoSelecionado;
+
+        switch (tipo) {
+            case 'nacional':
+                tipoSelecionado = PNF.NATIONAL;
+                break;
+            case 'internacional':
+                tipoSelecionado = PNF.INTERNATIONAL;
+                break;
+            case 'E164':
+                tipoSelecionado = PNF.E164;
+                break;
+            case 'RFC3966':
+                tipoSelecionado = PNF.RFC3966;
+                break;
+            default:
+                tipoSelecionado = PNF.NATIONAL;
+                break;
+        }
 
         // Format the number.
-        let parsedPhoneObj = phoneUtil.parse(newValue, 'US');
-
-        return phoneUtil.format(parsedPhoneObj, PNF.INTERNATIONAL);
+        let parsedPhoneObj = phoneUtil.parse(value, country);
+        return phoneUtil.format(parsedPhoneObj, tipoSelecionado);
 
     }
 }
